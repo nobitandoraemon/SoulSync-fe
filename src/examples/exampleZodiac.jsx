aeda7d55a52d318084b54e0dce6884a6373b23a6
Tenzo <84665790+1020phug@users.noreply.github.com>
2025-02-26 11:48:40 +0700

    Add example data for zodiac

---
+++ src/examples/exampleZodiac.jsx
@@ -0,0 +1,56 @@
+const Aries = [
+	{
+		id: 1,
+		overall: {
+			gioithieu: "",
+			nguyento: "",
+			tinhcach: "",
+			other: "",
+		},
+		truyenthuyet: "",
+		tinhcach: [
+			{ title: "tinh cach 1", content: "...." },
+			{ title: "tinh cach 2", content: "...." },
+			{ title: "tinh cach 3", content: "...." },
+			{ title: "tinh cach 4", content: "...." },
+		],
+		sunghiep: "",
+		tinhyeu: [
+			{ title: "tinh yeu 1", content: "...." },
+			{ title: "tinh yeu 2", content: "...." },
+			{ title: "tinh yeu 3", content: "...." },
+		],
+		matching: [
+			{ title: "cung 1", content: "...." },
+			{ title: "cung 2", content: "...." },
+			{ title: "cung 3", content: "...." },
+		],
+	},
+	{
+		id: 2,
+		overall: {
+			gioithieu: "",
+			nguyento: "",
+			tinhcach: "",
+			other: "",
+		},
+		truyenthuyet: "",
+		tinhcach: [
+			{ title: "tinh cach 1", content: "...." },
+			{ title: "tinh cach 2", content: "...." },
+			{ title: "tinh cach 3", content: "...." },
+			{ title: "tinh cach 4", content: "...." },
+		],
+		sunghiep: "",
+		tinhyeu: [
+			{ title: "tinh yeu 1", content: "...." },
+			{ title: "tinh yeu 2", content: "...." },
+			{ title: "tinh yeu 3", content: "...." },
+		],
+		matching: [
+			{ title: "cung 1", content: "...." },
+			{ title: "cung 2", content: "...." },
+			{ title: "cung 3", content: "...." },
+		],
+	},
+];
