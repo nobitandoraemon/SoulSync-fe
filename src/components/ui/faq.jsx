cbdd6f209df8a0f6613b1f9fd8275b8d5c5d400a
unknown <trinhhuylong2005@gmail.com>
2025-02-26 10:05:01 +0700

    old change before main merge

--- src/components/ui/faq.jsx
+++ src/components/ui/faq.jsx
@@ -1,3 +1,16 @@
+const QuestionBox = ({Question, Answer}) =>{
+	return <div>
+			<div className="collapse collapse-arrow bg-base-200">
+				<input type="radio" name="my-accordion-2" defaultChecked />
+				<div className="collapse-title text-xl font-medium">{Question}</div>
+				<div className="collapse-content">
+					<p>{Answer}</p>
+				</div>
+			</div>
+	</div>
+}
+
+
 const FAQ = () => {
 	return <div>
 		<section className="dark:bg-gray-100 dark:text-gray-800">
@@ -5,18 +18,9 @@ const FAQ = () => {
 				<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
 				<p className="mt-4 mb-8 dark:text-gray-600">Sagittis tempor donec id vestibulum viverra. Neque condimentum primis orci at lacus amet bibendum.</p>
 				<div className="space-y-4">
-					<details className="w-full border rounded-lg">
-						<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Ex orci laoreet egestas sapien magna egestas scelerisque?</summary>
-						<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-600">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
-					</details>
-					<details className="w-full border rounded-lg">
-						<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Lorem at arcu rutrum viverra metus sapien venenatis lobortis odio?</summary>
-						<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-600">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
-					</details>
-					<details className="w-full border rounded-lg">
-						<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Eleifend feugiat sollicitudin laoreet adipiscing bibendum suscipit erat?</summary>
-						<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-600">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
-					</details>
+					<QuestionBox Question="THIS IS YOUR QUESTION" Answer="THIS IS MY ANSWER"/>
+					<QuestionBox Question="THIS IS YOUR QUESTION" Answer="THIS IS MY ANSWER"/>
+					<QuestionBox Question="THIS IS YOUR QUESTION" Answer="THIS IS MY ANSWER"/>
 				</div>
 			</div>
 		</section>
