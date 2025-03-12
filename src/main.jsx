c81f4be5e2433c6c78b78a034d8011656163c697
Tenzo <84665790+1020phug@users.noreply.github.com>
2025-03-12 09:28:06 +0700

    Rebuild pages

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ import {
 	PrivateRoute,
 	NotFound,
 } from "./config/components.js";
-import TestPage from "./pages/test.jsx";
 import { socket } from "./config/socket.js";
 
 createRoot(document.getElementById("root")).render(
@@ -27,7 +26,6 @@ createRoot(document.getElementById("root")).render(
 							<Route path="/login" element={<Login />} />
 							<Route path="/reg" element={<Reg />} />
 							<Route path="*" element={<NotFound />} />
-							<Route path="/test" element={<TestPage />} />
 						</Route>
 						<Route element={<PrivateRoute />}>
 							<Route path="/chat" element={<Chat socket={socket} />} />
