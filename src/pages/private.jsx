4b2d6c7a56d9cac9e9daa8b06ebd175203b87309
Tenzo <84665790+1020phug@users.noreply.github.com>
2025-03-13 15:21:34 +0700

    Chat app beta1

--- src/pages/private.jsx
+++ src/pages/private.jsx
@@ -1,16 +1,15 @@
 import { Navigate, Outlet } from "react-router";
-import { useUser } from "../hooks/useUser";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 const PrivateRoute = () => {
-	const { user } = useUser();
+	const token = localStorage.getItem("token");
 	useEffect(() => {
-		if (!user) {
+		if (!token) {
 			toast("You need to log in", { type: "warning" });
 			setTimeout(() => {}, 3000);
 			return <Navigate to="/login" replace />;
 		}
-	}, [user]);
+	}, [token]);
 	return <Outlet />;
 };
 
