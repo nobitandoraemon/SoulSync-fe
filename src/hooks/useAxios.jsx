5d76288a551cb2a5b25771ec2285a8c24bd2d7d0
Tenzo <84665790+1020phug@users.noreply.github.com>
2025-03-13 11:12:19 +0700

    Fix bearer token b4

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const token = localStorage.getItem("token");
 const API = "https://soulsync-api.onrender.com";
 
 const useAxios = axios.create({
 	baseURL: API,
 	timeout: 1000,
-	headers: { authorization: `Bearer ${token}` },
+	headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
 });
 
 // // Add a request interceptor
