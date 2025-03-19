74ce7e6589f9d74b4fe70d77c33ecd6ebcc6b336
Master-Long-3112 <trinhhuylong2005@gmail.com>
2025-03-19 20:04:42 +0700

    upload

--- src/components/ui/chatpage/chatcontainer.jsx
+++ src/components/ui/chatpage/chatcontainer.jsx
@@ -1,51 +1,58 @@
 import { ChatBox, Content, Waiting } from "../../../config/components";
-
+import { useNavigate } from "react-router";
 const ChatContainer = ({
-	socketIO,
-	content,
-	isActive,
-	setActive,
-	toggleActive,
-	isScroll,
-	user,
-	matchedUser,
-	requestMatch,
-	chat,
-	ok,
-	setOk,
+  socketIO,
+  content,
+  isActive,
+  setActive,
+  toggleActive,
+  isScroll,
+  user,
+  matchedUser,
+  requestMatch,
+  chat,
+  ok,
+  setOk,
 }) => {
-	return (
-		<>
-			{
-				// Chưa có người sẽ match
-				!matchedUser ? (
-					<div className="max-w-[80%] mx-auto flex items-center justify-between">
-						<button className="btn btn-primary">Find a Match</button>
-					</div>
-				) : // Khi chấp nhận chat
-				ok ? (
-					<>
-						{/* <Content
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/match");
+  };
+  return (
+    <>
+      {
+        // Chưa có người sẽ match
+        !matchedUser ? (
+          <div className="max-w-[80%] mx-auto flex items-center justify-between">
+            <button
+              onClick={() => handleNavigate()}
+              className="btn btn-primary"
+            >
+              Find a Match
+            </button>
+          </div>
+        ) : (
+          // Khi chấp nhận chat
+          <>
+            {/* <Content
 							user={user}
 							content={content}
 							isActive={isActive}
 							isScroll={isScroll}
 							requestMatch={requestMatch}
 						/> */}
-						<ChatBox
-							chat={chat}
-							user={user}
-							socketIO={socketIO}
-							matchedUser={matchedUser}
-						/>
-					</>
-				) : (
-					//Vào phòng đợi, yêu cầu chấp nhận khi được hỏi
-					<Waiting ok={ok} setOk={setOk} requestMatch={requestMatch} />
-				)
-			}
-		</>
-	);
+            <ChatBox
+              chat={chat}
+              user={user}
+              socketIO={socketIO}
+              matchedUser={matchedUser}
+              content={content}
+            />
+          </>
+        )
+      }
+    </>
+  );
 };
 
 export default ChatContainer;
--- src/pages/match.jsx
+++ src/pages/match.jsx
@@ -1,8 +1,42 @@
 import { useOutletContext } from "react-router";
+import ZodiacInfo from "../components/ui/zodiacinfo";
+import { zodiacInfo } from "../lib/data";
+import { FaFacebookF, FaGithub, FaInstagram } from "react-icons/fa6";
+import { TbZodiacLibra } from "react-icons/tb";
+const testuser = {
+  main: true,
+  name: "Phùng Quang Anh",
+  job: "Website Developer",
+  location: "Hà Nội, VN",
+  id: "1020",
+  zodiac: <TbZodiacLibra />,
+  age: 25,
+  avatar:
+    "https://media.daily.dev/image/upload/s--wzOhK88f--/f_auto/v1724228753/avatars/avatar_nyNDZ2Trf7sk4FgOodgWN",
+  social: [
+    {
+      icon: <FaFacebookF />,
+      href: "https://fb.me/1020phug",
+    },
+    {
+      icon: <FaGithub />,
+      href: "https://github.com/1020phug",
+    },
+    {
+      icon: <FaInstagram />,
+      href: "https://instagram.com/1020phug",
+    },
+  ],
+  quote: `Cause when a heart break, and it don't break even`,
+};
 const MatchPage = () => {
-	const [user] = useOutletContext();
-	console.log(user);
-	return <div>Match Page here</div>;
+  const [user] = useOutletContext();
+  console.log(user);
+  return (
+    <>
+      <ZodiacInfo zodiac={zodiacInfo[0]} user={testuser} />
+    </>
+  );
 };
 
 export default MatchPage;
