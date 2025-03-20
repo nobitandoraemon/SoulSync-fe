0d80f04655da380d2f3f6fda914ef0975f0feb90
jBoy2k05 <truonggioitoan@gmail.com>
2025-03-20 14:29:29 +0700

    fix axios

--- src/pages/private.jsx
+++ src/pages/private.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, Outlet } from "react-router";
 import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
-import { APP_ROUTES } from "../lib/constants";
+import { API_ROUTES, APP_ROUTES } from "../lib/constants";
 import axios from "axios";
 const PrivateRoute = () => {
 	const navigate = useNavigate();
@@ -16,22 +16,32 @@ const PrivateRoute = () => {
 				navigate(APP_ROUTES.SIGN_IN);
 			}, 1500);
 		} else {
-      axios
-        .get(`https://soulsync-api.onrender.com/users/${formData.username}`) 
-        .then((response) => {
-					if (!response.data.gender || !response.data.birthday || !response.data.zodiac) {
+			const getData = async () => {
+				try {
+					const response = await axios({
+						method: "GET",
+						url: API_ROUTES.GET_USER + `/${username}`,
+						headers: {
+							authorization: `Bearer ${token}`,
+							"Content-Type": "application/json",
+						},
+						withCredentials: true,
+					});
+					console.log(response);
+					if (!response.data.gender || !response.data.birthday || !response.data.zodiac ) {
 						toast("Please fill out your missing information", { type: "warning" });
-						setTimeout(() => {
-							navigate(APP_ROUTES.FORM); // Replace with your form route
-						}, 1500);
+			      setTimeout(() => {
+				      navigate(APP_ROUTES.FORM);
+			}, 1500);
 					}
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-			
-    }
-	}, []);
+				} catch (error) {
+					console.error("Error fetching data:", error);
+				}
+			};
+	
+			getData(); // Call the function
+		}
+	}, [navigate, token]);
 	
 	return <Outlet />;
 };
