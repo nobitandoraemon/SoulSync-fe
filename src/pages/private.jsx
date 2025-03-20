8215925cb946b55031cb6975f87b049f1f2438b5
jBoy2k05 <truonggioitoan@gmail.com>
2025-03-20 19:28:02 +0700

    fix private

--- src/pages/private.jsx
+++ src/pages/private.jsx
@@ -1,26 +1,49 @@
 import { useNavigate, Outlet } from "react-router";
 import { toast } from "react-toastify";
-import { useEffect } from "react";
-import { APP_ROUTES } from "../lib/constants";
+import { useEffect, useState } from "react";
+import { API_ROUTES, APP_ROUTES } from "../lib/constants";
+import axios from "axios";
 const PrivateRoute = () => {
 	const navigate = useNavigate();
 	const token = localStorage.getItem("token");
+	const username = localStorage.getItem("username");
+
+	
+
 	useEffect(() => {
 		if (!token) {
 			toast("You are not logged in", { type: "warning" });
 			setTimeout(() => {
 				navigate(APP_ROUTES.SIGN_IN);
 			}, 1500);
-
-			// } else {
-			// 	if (!user.gender) {
-			// 		toast("You are not fulfil your information!", { type: "warning" });
-			// 		setTimeout(() => {
-			// 			navigate(APP_ROUTES.FORM);
-			// 		}, 3000);
-			// 	}
+		} else {
+			const getData = async () => {
+				try {
+					const response = await axios({
+						method: "GET",
+						url: API_ROUTES.GET_USER + `/${username}`,
+						headers: {
+							authorization: `Bearer ${token}`,
+							"Content-Type": "application/json",
+						},
+						withCredentials: true,
+					});
+					console.log(response);
+					if (!response.data.gender || !response.data.birthday || !response.data.zodiac  ) {
+						toast("Please fill out your missing information", { type: "warning" });
+			      setTimeout(() => {
+				      navigate(APP_ROUTES.FORM);
+			}, 1500);
+					}
+				} catch (error) {
+					console.error("Error fetching data:", error);
+				}
+			};
+	
+			getData(); // Call the function
 		}
-	}, []);
+	}, [navigate, token]);
+	
 	return <Outlet />;
 };
 
