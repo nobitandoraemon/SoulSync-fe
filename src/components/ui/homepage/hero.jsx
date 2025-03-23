bd6fe4df24e3fcf9b48314baec5d6f3f32bfcc85
jBoy2k05 <truonggioitoan@gmail.com>
2025-03-23 10:58:25 +0700

    update layout hero section

--- src/components/ui/homepage/hero.jsx
+++ src/components/ui/homepage/hero.jsx
@@ -7,27 +7,21 @@ const Hero = () => {
 	return (
 		<div className="container mx-auto">
 			<div className="min-h-screen hero">
-				<div className="flex-col justify-between hero-content lg:flex-row-reverse">
-					<div className="w-3/4">
-						<img src={image} alt="phone" className="w-full" />
-					</div>
-					<div className="flex flex-col items-center justify-center md:items-start">
-						<div className="mb-4 badge badge-accent">Brand new</div>
-						<h1 className="w-3/4 text-4xl font-bold md:text-5xl text-wrap">
-							Tình Yêu Đích Thực 
-							<br />
-							<span className="text-primary">Chỉ Cần Một Cú Click!</span>
-						</h1>
-						<p className="w-3/4 py-6">
-						Khám phá thế giới hẹn hò thông minh với hệ thống ghép đôi chính xác, giúp bạn tìm thấy một nửa hoàn hảo dựa trên cung hoàng đạo, thần số học, vị trí địa lý và sở thích. Kết nối dễ dàng, trò chuyện ngay lập tức và hoàn toàn bảo mật thông tin cá nhân. Không còn những lần chờ đợi vô ích – tình yêu của bạn chỉ cách một cú click!
-						</p>
-						<div className="flex items-center gap-2">
+				<div className="flex justify-between items-center w-full">
+					<div className="flex flex-col gap-6 w-[60%]"> {/* description */}
+					  <div className="badge badge-accent ml-60  ">Brand new</div>
+					  <div className="flex flex-col gap-2 ">
+						<h1 className=" text-5xl font-bold md:text-6xl text-wrap text-[#f317fd]">Tình Yêu Đích Thực</h1>
+						<h1 className="text-primary text-5xl font-bold md:text-6xl ! ">Chỉ Cần Một Cú Click!</h1>
+					  </div>
+					  <p className="text-xl w-[90%]">Khám phá thế giới hẹn hò thông minh với hệ thống ghép đôi chính xác, giúp bạn tìm thấy một nửa hoàn hảo dựa trên cung hoàng đạo, thần số học, vị trí địa lý và sở thích. Kết nối dễ dàng, trò chuyện ngay lập tức và hoàn toàn bảo mật thông tin cá nhân. Không còn những lần chờ đợi vô ích – tình yêu của bạn chỉ cách một cú click!</p>
+					  <div className="flex items-center gap-2">
 							{!token ? (
 								<>
-									<Link to="/login" className="btn btn-primary">
+									<Link to="/login" className="btn btn-success w-1/4 bg-green-400 text-white">
 										Đăng nhập
 									</Link>
-									<Link to="/reg" className="btn btn-secondary">
+									<Link to="/reg" className="btn btn-secondary w-1/4 bg-pink-100 text-violet-500">
 										Đăng ký
 									</Link>
 								</>
@@ -37,6 +31,9 @@ const Hero = () => {
 								</Link>
 							)}
 						</div>
+					</div>
+					<div className="w-[45%]">
+						<img src={image} alt="phone" className="w-full" />
 					</div>
 				</div>
 			</div>
