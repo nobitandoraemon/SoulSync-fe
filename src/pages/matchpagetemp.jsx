41e42d1676397d87488689a84991d5e8cc1481a7
Master-Long-3112 <trinhhuylong2005@gmail.com>
2025-03-26 07:36:59 +0700

    update matchPageTest

--- src/pages/matchpagetemp.jsx
+++ src/pages/matchpagetemp.jsx
@@ -2,43 +2,49 @@ import { useState } from "react";
 import { zodiacInfo } from "../lib/data";
 import { Link } from "react-router";
 import { logOut } from "../lib/common";
-const Tab = ({ tab }) => (
-  <>
-    <div className="bg-base-100 border-base-300 p-6 h-[80vh] rounded-3xl overflow-y-auto break-words ">
-      {tab.content.map((item, index) => {
-        return (
-          <ul key={index}>
-            {item.type === "text" ? (
-              <>
-                <li>{item.value}</li>
-                <br />
-              </>
-            ) : item.type === "image" ? (
-              <li className="">
-                <img
-                  src={item.value}
-                  alt="Zodiac"
-                  className="w-full aspect-video"
-                />
-              </li>
-            ) : item.type === "h2" ? (
-              <li>
-                <h2 className=" text-xl font-bold">{item.value}</h2>
-              </li>
-            ) : null}
-          </ul>
-        );
-      })}
-    </div>
-  </>
-);
-const MatchPageTest = () => {
-  const [tabActive, setTabActive] = useState(0);
-  const zodiac = zodiacInfo[24 - 1];
+import { useOutletContext } from "react-router";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import Toast from "../hooks/useToast";
+const SideBarNew = ({ setTabActive, user, handleFinding }) => {
   return (
     <div className="flex flex-row">
       <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
         <div className="flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+          <div>
+            <div className="w-full">
+              <div className="relative w-full bg-gray-50 flex justify-center items-center">
+                <div className="absolute flex justify-center items-center">
+                  <img
+                    className="object-cover h-20 w-20 rounded-full"
+                    src="./src/assets/12-cung-hoang-dao-thang-sinh-1.jpg"
+                    alt=""
+                  />
+                </div>
+
+                <div className="h-56 mx-4 w-5/6 bg-blue-400 rounded-3xl shadow-md sm:w-80 sm:mx-0 ">
+                  <div className="h-1/2 w-full flex justify-between items-baseline px-3 py-5">
+                    <h1 className="text-white text-center w-full py-5">
+                      User Profile
+                    </h1>
+                  </div>
+
+                  <div className=" bg-white h-1/2 w-full  rounded-3xl  flex flex-col  justify-around  items-center">
+                    <div className="w-full h-1/2 flex justify-between items-center px-3 pt-2"></div>
+                    <div className="w-full h-1/2 flex flex-col justify-center items-center">
+                      <h1 className="text-gray-700 font-bold">
+                        {/*user.fullName*/} Master
+                      </h1>
+                      <h1 className="text-gray-500 text-sm">
+                        {/*user.place*/} Phu Tho
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*Bar*/}
+          </div>
           <div className="relative overflow-x-hidden flex">
             <ul className="flex flex-col py-4 space-y-1">
               <li className="px-5">
@@ -199,26 +205,135 @@ const MatchPageTest = () => {
               </li>
             </ul>
           </div>
+          <button
+            className="mx-auto my-5 p-6 shadow-sm btn-circle btn btn-primary animate-bounce size-28"
+            onClick={handleFinding}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              strokeWidth="2.5"
+              stroke="currentColor"
+              className="size-48"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-      <div
-        className="min-h-screen  hero"
-        // style={{
-        // 	backgroundImage:
-        // 		"url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-        // 	backgroundPosition: "center",
-        // 	backgroundSize: "cover",
-        // }}
-      >
-        <div className="hero-overlay bg-primary w-full h-full"></div>
-        <div className="w-4/5 hero-content bg-base-200 rounded-xl bg-opacity-60">
-          <div className="flex flex-col items-center">
-            <div className="">
-              <Tab tab={zodiac.tabs[tabActive]} />;
-            </div>
+    </div>
+  );
+};
+
+const UserZodiacInfomation = ({ zodiac, tabActive }) => {
+  return (
+    <div
+      className="min-h-screen  hero"
+      // style={{
+      // 	backgroundImage:
+      // 		"url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      // 	backgroundPosition: "center",
+      // 	backgroundSize: "cover",
+      // }}
+    >
+      <div className="hero-overlay bg-primary w-full h-full"></div>
+      <div className="w-4/5 hero-content bg-base-200 rounded-xl bg-opacity-60">
+        <div className="flex flex-col items-center">
+          <div className="">
+            <Tab tab={zodiac.tabs[tabActive]} />;
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+const Tab = ({ tab }) => (
+  <>
+    <div className="bg-base-100 border-base-300 p-6 h-[80vh] rounded-3xl overflow-y-auto break-words ">
+      {tab.content.map((item, index) => {
+        return (
+          <ul key={index}>
+            {item.type === "text" ? (
+              <>
+                <li>{item.value}</li>
+                <br />
+              </>
+            ) : item.type === "image" ? (
+              <li className="">
+                <img
+                  src={item.value}
+                  alt="Zodiac"
+                  className="w-full aspect-video"
+                />
+              </li>
+            ) : item.type === "h2" ? (
+              <li>
+                <h2 className=" text-xl font-bold">{item.value}</h2>
+              </li>
+            ) : null}
+          </ul>
+        );
+      })}
+    </div>
+  </>
+);
+const MatchPageTest = () => {
+  const { user, isFinding, setIsFinding } = useOutletContext();
+  const [tabActive, setTabActive] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const handleFinding = () => {
+    setIsFinding((prev) => !prev);
+    setTimeout(() => {
+      navigate(APP_ROUTES.CHAT);
+    }, 1000);
+  };
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (user) {
+      console.log("Get user data successfully");
+      if (!user.gender) {
+        toast("Bạn thiếu thông tin để có thể phân tích dữ liệu ...", {
+          type: "warning",
+        });
+        setTimeout(() => {
+          navigate(APP_ROUTES.FORM);
+        }, 2000);
+      }
+    } else {
+      console.log("Get user data failed");
+    }
+  }, [user]);
+
+  useEffect(() => {
+    setInterval(() => {
+      handleLoading();
+    }, 5000);
+    return () => clearInterval(handleLoading);
+  }, []);
+  //const zodiac = zodiacInfo[user.zodiac-1];
+  //test zodiac
+  const zodiac = zodiacInfo[24 - 1];
+  return isLoading ? (
+    <div className="flex items-center justify-center w-screen h-screen ">
+      <Toast />
+      <span className="text-2xl animate-pulse">
+        Phân tích cung hoàng đạo ...
+      </span>
+      <span className="ml-8 loading loading-spinner text-info"></span>
+    </div>
+  ) : (
+    <div className="flex flex-row">
+      <Toast />
+      <SideBarNew setTabActive={setTabActive} handleFinding={handleFinding} />
+      <UserZodiacInfomation tabActive={tabActive} zodiac={zodiac} />
     </div>
   );
 };
