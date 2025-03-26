a3fbbf99b562b5ef2165f776f19cd0a09b604298
1020phug <1020phug@gmail.com>
2025-03-26 07:14:53 +0700

    Completed setting page

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,18 +1,166 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate, useOutletContext } from "react-router";
 import { toast } from "react-toastify";
-import { APP_ROUTES } from "../lib/constants";
+import { API_ROUTES, APP_ROUTES } from "../lib/constants";
 import { cn } from "../lib/utils";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage, responsive, placeholder } from "@cloudinary/react";
+import axios from "axios";
+import { getTokenFromLocalStorage } from "../lib/common";
+import { image } from "@cloudinary/url-gen/qualifiers/source";
+import dayjs from "dayjs";
+import Toast from "../hooks/useToast";
+import { VietnamProvinces, zodiacInfo } from "../lib/data";
+
+const ChangeImage = ({ formData, setFormData }) => {
+	const CloudinaryUploadWidget = ({
+		uwConfig,
+		setPublicId,
+		formData,
+		setFormData,
+	}) => {
+		const uploadWidgetRef = useRef(null);
+		const uploadButtonRef = useRef(null);
+
+		useEffect(() => {
+			const initializeUploadWidget = () => {
+				if (window.cloudinary && uploadButtonRef.current) {
+					// Create upload widget
+					uploadWidgetRef.current = window.cloudinary.createUploadWidget(
+						uwConfig,
+						(error, result) => {
+							if (!error && result && result.event === "success") {
+								console.log("Upload successful:", result.info);
+								setFormData({ ...formData, image: result.info.secure_url });
+								console.log(formData);
+								setPublicId(result.info.public_id);
+							}
+						}
+					);
+
+					// Add click event to open widget
+					const handleUploadClick = () => {
+						if (uploadWidgetRef.current) {
+							uploadWidgetRef.current.open();
+						}
+					};
+
+					const buttonElement = uploadButtonRef.current;
+					buttonElement.addEventListener("click", handleUploadClick);
+
+					// Cleanup
+					return () => {
+						buttonElement.removeEventListener("click", handleUploadClick);
+					};
+				}
+			};
+
+			initializeUploadWidget();
+		}, [uwConfig, setPublicId]);
+
+		return (
+			<button
+				ref={uploadButtonRef}
+				id="upload_widget"
+				className="btn btn-primary btn-wide"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="size-4"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+					/>
+				</svg>
+				Upload
+			</button>
+		);
+	};
+	// Configuration
+	const cloudName = "dbpqg1gsv";
+	const uploadPreset = "soulsync";
+
+	// State
+	const [publicId, setPublicId] = useState("");
+
+	// Cloudinary configuration
+	const cld = new Cloudinary({
+		cloud: {
+			cloudName,
+		},
+	});
+
+	// Upload Widget Configuration
+	const uwConfig = {
+		cloudName,
+		uploadPreset,
+		// Uncomment and modify as needed:
+		cropping: true,
+		showAdvancedOptions: true,
+		sources: ["local", "url"],
+		multiple: false,
+		folder: "avatars",
+		tags: ["users", "profile"],
+		context: { alt: "user_uploaded" },
+		// clientAllowedFormats: ["images"],
+		maxImageFileSize: 2000000,
+		// maxImageWidth: 2000,
+		// theme: 'purple',
+	};
+
+	return (
+		<div>
+			<CloudinaryUploadWidget
+				uwConfig={uwConfig}
+				setPublicId={setPublicId}
+				formData={formData}
+				setFormData={setFormData}
+			/>
+		</div>
+	);
+};
 
 const DangerZone = ({ user }) => {
 	const [show, setShow] = useState(false);
+	const token = getTokenFromLocalStorage();
+	const handleDelete = async (e) => {
+		e.preventDefault();
 
+		try {
+			const response = await axios({
+				method: "DELETE",
+				url: API_ROUTES.GET_USER + `/${user.username}`,
+				headers: {
+					authorization: `Bearer ${token}`,
+				},
+				withCredentials: true,
+			});
+			toast(response.data.message, { type: "success" });
+			setTimeout(() => {
+				location.reload();
+			}, 1500);
+		} catch (error) {
+			toast(error.data.message, { type: "error" });
+		}
+	};
 	return (
-		<div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-			<h2 className="pl-6 text-2xl font-bold sm:text-xl">Danger Zone</h2>
+		<div
+			className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg"
+			id="danger"
+			onSubmit={handleDelete}
+		>
+			<h2 className="pl-6 text-2xl font-bold sm:text-xl text-primary">
+				Danger Zone
+			</h2>
 
-			<div className="grid max-w-2xl mx-auto mt-8">
-				<div className="items-center mt-8 sm:mt-14 mx-auto md:mx-0">
+			<form className="grid max-w-2xl mx-auto mt-8">
+				<div className="items-center mx-auto mt-8 sm:mt-14 md:mx-0">
 					<div className="inline-grid *:[grid-area:1/1]">
 						<div
 							className={cn("status animate-ping", {
@@ -42,7 +190,7 @@ const DangerZone = ({ user }) => {
 						<div role="alert" className="alert alert-warning">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
-								className="h-6 w-6 shrink-0 stroke-current"
+								className="w-6 h-6 stroke-current shrink-0"
 								fill="none"
 								viewBox="0 0 24 24"
 							>
@@ -55,17 +203,20 @@ const DangerZone = ({ user }) => {
 							</svg>
 							<span>Cảnh báo: Toàn bộ dữ liệu của bạn có thể mất</span>
 						</div>
-						<button className="btn btn-error">Delete</button>
+						<button type="submit" className="btn btn-error">
+							Delete
+						</button>
 					</div>
 				)}
-			</div>
+			</form>
 		</div>
 	);
 };
 
-const Others = ({ user, formData, setFormData }) => {
+const Others = ({ user, formData, setFormData, handleSubmit }) => {
 	const [userHobby, setUserHobby] = useState([]);
 	const [hobbiesChange, setHobbiesChange] = useState(false);
+	const [locationChange, setLocationChange] = useState(false);
 	const handleHobbyChange = (hobby) => {
 		let updatedHobby;
 		if (userHobby.includes(hobby)) {
@@ -76,6 +227,13 @@ const Others = ({ user, formData, setFormData }) => {
 		setUserHobby(updatedHobby);
 		setFormData({ ...formData, hobbies: updatedHobby });
 	};
+	const Locations = VietnamProvinces.map((location) => {
+		return (
+			<option key={location} value={location}>
+				{location}
+			</option>
+		);
+	});
 	const ChangeHobbies = ({
 		user,
 		userHobby,
@@ -84,7 +242,7 @@ const Others = ({ user, formData, setFormData }) => {
 		setFormData,
 	}) => {
 		return (
-			<div className="flex gap-1 justify-center items-center flex-wrap">
+			<div className="flex flex-wrap items-center justify-center gap-1">
 				<button
 					type="button"
 					className={`w-[32%] text-black font-semibold border border-gray-300 rounded-full p-3 cursor-pointer ${
@@ -171,19 +329,103 @@ const Others = ({ user, formData, setFormData }) => {
 	};
 
 	return (
-		<div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-			<h2 className="pl-6 text-2xl font-bold sm:text-xl">Others</h2>
+		<form
+			className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg"
+			onSubmit={handleSubmit}
+			id="others"
+		>
+			<h2 className="pl-6 text-2xl font-bold sm:text-xl text-primary">
+				Others
+			</h2>
 
 			<div className="grid max-w-2xl mx-auto">
 				<div className="items-center mt-8 sm:mt-14">
 					<div className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
+						<div className="w-full mb-6">
+							<fieldset className="fieldset">
+								<legend className="w-full fieldset-legend">
+									Số điện thoại của bạn
+									<div className="btn btn-sm btn-warning btn-outline" disabled>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="size-4"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+											/>
+										</svg>
+									</div>
+								</legend>
+								<input
+									type="number"
+									className="input"
+									minLength={10}
+									maxLength={11}
+									placeholder={user.phoneNumber}
+									onChange={(e) =>
+										setFormData({ ...formData, phoneNumber: e.target.value })
+									}
+								/>
+							</fieldset>
+						</div>
+						<div className="w-full mb-6">
+							<fieldset className="fieldset">
+								<legend className="w-full fieldset-legend">
+									Địa chỉ
+									<button
+										className={cn("btn btn-sm btn-outline", {
+											"btn-warning": locationChange === false,
+											"btn-success": locationChange === true,
+										})}
+										onClick={(e) => {
+											e.preventDefault();
+											setLocationChange((prev) => !prev);
+										}}
+									>
+										{!locationChange ? "Sửa" : "Quay lại"}
+									</button>
+								</legend>
+								{!locationChange && (
+									<input
+										type="text"
+										className="input"
+										placeholder={user.location}
+									/>
+								)}
+								{locationChange && (
+									<select
+										className="select select-success"
+										defaultChecked={user.location}
+										onChange={(e) =>
+											setFormData({ ...formData, location: e.target.value })
+										}
+									>
+										{Locations}
+									</select>
+								)}
+							</fieldset>
+						</div>
+					</div>
+					<div className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
 						<div className="flex flex-col gap-2">
-							<fieldset className="fieldset w-full">
-								<legend className="fieldset-legend w-full">
+							<fieldset className="w-full fieldset">
+								<legend className="w-full fieldset-legend">
 									Sở thích của bạn
 									<button
-										className="btn btn-info"
-										onClick={() => setHobbiesChange((prev) => !prev)}
+										className={cn("btn btn-outline", {
+											"btn-warning": hobbiesChange === false,
+											"btn-success": hobbiesChange === true,
+										})}
+										onClick={(e) => {
+											e.preventDefault();
+											setHobbiesChange((prev) => !prev);
+										}}
 									>
 										{!hobbiesChange ? "Sửa" : "Quay lại"}
 									</button>
@@ -197,7 +439,7 @@ const Others = ({ user, formData, setFormData }) => {
 										setFormData={setFormData}
 									/>
 								) : (
-									<div className="flex gap-1 justify-center items-center flex-wrap">
+									<div className="flex flex-wrap items-center justify-center gap-1">
 										<button
 											type="button"
 											className={`w-[32%] text-black font-semibold border border-gray-300 rounded-full p-3 cursor-pointer ${
@@ -295,63 +537,94 @@ const Others = ({ user, formData, setFormData }) => {
 						</div>
 					</div>
 
-					<div className="mb-6 w-full">
-						<fieldset className="fieldset w-full">
+					<div className="w-full mb-6">
+						<fieldset className="w-full fieldset">
 							<legend className="fieldset-legend">
 								Câu trích dẫn yêu thích của bạn
 							</legend>
 							<textarea
-								className="textarea h-24 w-full"
+								className="w-full h-24 textarea"
 								placeholder={user.quote}
+								onChange={(e) =>
+									setFormData({ ...formData, quote: e.target.value })
+								}
 							></textarea>
 							<div className="fieldset-label">Optional</div>
 						</fieldset>
 					</div>
 
 					<div className="flex justify-end">
-						<button
-							type="submit"
-							className="py-3.5 px-7 cursor-pointer text-primary-content font-medium focus:outline-none bg-primary rounded-lg border border-indigo-200 hover:bg-secondary/60 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
-						>
+						<button type="submit" className="btn btn-primary btn-soft">
 							Lưu
 						</button>
 					</div>
 				</div>
 			</div>
-		</div>
+		</form>
 	);
 };
 
-const Public = ({ user, formData, setFormData }) => {
-	return (
-		<div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-			<h2 className="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
+const Public = ({ user, formData, setFormData, handleSubmit }) => {
+	const [type, setType] = useState("text");
 
-			<div className="grid max-w-2xl mx-auto mt-8">
-				<div className="flex flex-col items-center justify-center md:justify-between space-y-5 sm:flex-row sm:space-y-0">
-					<div className="avatar">
-						<div className="ring-primary ring-offset-base-100 w-40 h-40 avatar-online rounded-full ring ring-offset-2">
-							<img src={user.image} />
+	const handleTemp = (e) => {
+		e.preventDefault();
+	};
+	const DeleteImage = () => {
+		setFormData({
+			...formData,
+			image:
+				"https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png",
+		});
+	};
+	return (
+		<div
+			className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg"
+			id="public"
+		>
+			<h2 className="pl-6 text-2xl font-bold sm:text-xl text-primary">
+				Public Profile
+			</h2>
+
+			<form className="grid max-w-2xl mx-auto mt-8" onSubmit={handleSubmit}>
+				<div className="flex flex-col items-center justify-center space-y-5 md:justify-between sm:flex-row sm:space-y-0">
+					<div className="avatar indicator">
+						<span className="indicator-item badge badge-lg badge-soft badge-neutral">
+							{zodiacInfo[user.zodiac].icon}
+							{zodiacInfo[user.zodiac].sign}
+						</span>
+						<div className="w-40 h-40 rounded-full ring-primary ring-offset-base-100 ring ring-offset-2">
+							{formData.image ? (
+								<img src={formData.image} />
+							) : (
+								<img src={user.image} />
+							)}
 						</div>
 					</div>
 
 					<div className="flex flex-col space-y-5 sm:ml-8">
+						<form action="#">
+							<input
+								type="text"
+								name="avatar-link"
+								placeholder="Avatar Link"
+								className="hidden"
+								required
+								onChange={handleTemp}
+							/>
+							<ChangeImage formData={formData} setFormData={setFormData} />
+						</form>
 						<button
 							type="button"
-							className="py-3.5 px-7 text-primary-content font-medium focus:outline-none bg-primary rounded-lg border border-indigo-200 hover:bg-secondary/60 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
-						>
-							Change picture
-						</button>
-						<button
-							type="button"
-							className="py-3.5 px-7 text-base font-medium text-primary focus:outline-none bg-base-100 rounded-lg border border-indigo-200 hover:bg-accent/60 hover:text-accent-content focus:z-10 focus:ring-4 focus:ring-indigo-200 "
+							className="btn btn-primary btn-outline"
+							onClick={DeleteImage}
 						>
 							Delete picture
 						</button>
 					</div>
 				</div>
 
-				<div className="items-center mt-8 sm:mt-14 mx-auto md:mx-0">
+				<div className="items-center mx-auto mt-8 sm:mt-14 md:mx-0">
 					<div className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
 						<div className="w-full">
 							<fieldset className="fieldset">
@@ -360,6 +633,9 @@ const Public = ({ user, formData, setFormData }) => {
 									type="text"
 									className="input"
 									placeholder={user.fullName}
+									onChange={(e) =>
+										setFormData({ ...formData, fullName: e.target.value })
+									}
 								/>
 							</fieldset>
 						</div>
@@ -369,10 +645,20 @@ const Public = ({ user, formData, setFormData }) => {
 								<legend className="fieldset-legend">Ngày sinh của bạn</legend>
 
 								<input
-									type="date"
+									type={type}
+									onFocus={() => setType("date")}
+									onBlur={() => setType("text")}
 									className="input validator"
-									max="2007-12-31"
+									max="2010-12-31"
+									onChange={(e) =>
+										setFormData({ ...formData, birthday: e.target.value })
+									}
 									title="Must be valid URL"
+									placeholder={
+										formData.birthday
+											? dayjs(formData.birthday).format("DD/MM/YYYY")
+											: dayjs(user.birthday).format("DD/MM/YYYY")
+									}
 								/>
 							</fieldset>
 						</div>
@@ -428,15 +714,12 @@ const Public = ({ user, formData, setFormData }) => {
 					</div>
 
 					<div className="flex justify-end mt-16">
-						<button
-							type="submit"
-							className="py-3.5 px-7 cursor-pointer text-primary-content font-medium focus:outline-none bg-primary rounded-lg border border-indigo-200 hover:bg-secondary/60 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
-						>
+						<button type="submit" className="btn btn-primary btn-soft">
 							Lưu
 						</button>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 };
@@ -446,6 +729,30 @@ const SettingPage = () => {
 	const navigate = useNavigate();
 	const [id, setId] = useState(1);
 	const [formData, setFormData] = useState({});
+	const token = getTokenFromLocalStorage();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await axios({
+				method: "PUT",
+				url: API_ROUTES.GET_USER + `/${user.username}`,
+				headers: {
+					authorization: `Bearer ${token}`,
+				},
+				data: formData,
+				withCredentials: true,
+			});
+			toast("Cập nhật thành công", { type: "success" });
+			setTimeout(() => {
+				location.reload();
+			}, 1500);
+		} catch (error) {
+			console.error(error);
+			toast("Cập nhật thành bại", { type: "error" });
+		}
+	};
 	useEffect(() => {
 		if (user) {
 			console.log("Get user data successfully", user);
@@ -462,18 +769,21 @@ const SettingPage = () => {
 		}
 	}, [user]);
 	return !user ? (
-		<div className="flex min-h-screen max-w-3xl p-16 mx-auto flex-col gap-4">
-			<div className="skeleton h-32 w-full"></div>
-			<div className="skeleton h-4 w-28"></div>
-			<div className="skeleton h-4 w-full"></div>
-			<div className="skeleton h-4 w-full"></div>
+		<div className="flex flex-col max-w-3xl min-h-screen gap-4 p-16 mx-auto">
+			<div className="w-full h-32 skeleton"></div>
+			<div className="h-4 skeleton w-28"></div>
+			<div className="w-full h-4 skeleton"></div>
+			<div className="w-full h-4 skeleton"></div>
 		</div>
 	) : (
 		<>
 			<div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
+				<Toast />
 				<aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
 					<div className="sticky flex flex-col gap-2 p-4 text-sm border-r border-indigo-100 top-12">
-						<h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
+						<h2 className="pl-3 mb-4 text-2xl font-semibold text-primary">
+							Settings
+						</h2>
 
 						<li
 							onClick={() => setId(1)}
@@ -519,21 +829,23 @@ const SettingPage = () => {
 							<Public
 								user={user}
 								formData={formData}
-								setFormData={{ setFormData }}
+								setFormData={setFormData}
+								handleSubmit={handleSubmit}
 							/>
 						)}
 						{id === 2 && (
 							<Others
 								user={user}
 								formData={formData}
-								setFormData={{ setFormData }}
+								setFormData={setFormData}
+								handleSubmit={handleSubmit}
 							/>
 						)}
 						{id === 3 && (
 							<DangerZone
 								user={user}
 								formData={formData}
-								setFormData={{ setFormData }}
+								setFormData={setFormData}
 							/>
 						)}
 					</div>
