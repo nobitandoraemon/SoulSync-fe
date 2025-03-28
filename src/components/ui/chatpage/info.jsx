de6e068e7754f9e3308b84605cb62c00d404bc64
1020phug <1020phug@gmail.com>
2025-03-28 21:33:35 +0700

    Completed prj

--- src/components/ui/chatpage/info.jsx
+++ src/components/ui/chatpage/info.jsx
@@ -1,5 +1,6 @@
 import { m } from "motion/react";
 import { ZodiacSigns } from "../../../lib/data";
+import dayjs from "dayjs";
 
 const Info = ({ matchedUser, love, setLove, handleLike }) => {
 	return (
@@ -26,28 +27,76 @@ const Info = ({ matchedUser, love, setLove, handleLike }) => {
 							<div className="chat-bubble">{matchedUser.quote}</div>
 							<div className="opacity-50 chat-footer"> </div>
 						</div>
-						<p className="flex items-center my-4 text-secondary">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="size-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-								/>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-								/>
-							</svg>
-							{matchedUser.location}
-						</p>
+						<div className="flex items-center justify-around w-full">
+							<p className="flex items-center my-4 text-secondary">
+								{ZodiacSigns[matchedUser.zodiac - 1].symbol}
+								{ZodiacSigns[matchedUser.zodiac - 1].name}
+							</p>
+							<p className="flex items-center my-4 text-secondary">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth={1.5}
+									stroke="currentColor"
+									className="size-6"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+									/>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+									/>
+								</svg>
+								{matchedUser.location}
+							</p>
+						</div>
+						<div className="flex items-center justify-around w-full">
+							{love && (
+								<>
+									<p className="flex items-center my-4 text-secondary">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="size-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.871c1.355 0 2.697.056 4.024.166C17.155 8.51 18 9.473 18 10.608v2.513M15 8.25v-1.5m-6 1.5v-1.5m12 9.75-1.5.75a3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0L3 16.5m15-3.379a48.474 48.474 0 0 0-6-.371c-2.032 0-4.034.126-6 .371m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.169c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 0 1 3 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 0 1 6 13.12M12.265 3.11a.375.375 0 1 1-.53 0L12 2.845l.265.265Zm-3 0a.375.375 0 1 1-.53 0L9 2.845l.265.265Zm6 0a.375.375 0 1 1-.53 0L15 2.845l.265.265Z"
+											/>
+										</svg>
+
+										{dayjs(matchedUser.birthday).format("DD/MM/YYYY")}
+									</p>
+									<p className="flex items-center my-4 text-secondary">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="size-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+											/>
+										</svg>
+
+										{matchedUser.phoneNumber}
+									</p>
+								</>
+							)}
+						</div>
 					</div>
 					{love && (
 						<div className="mt-6">
